3fca0ab6c24d15e7c0d993374e59a7285fca61bb
Sven <svenwerner1997@gmail.com>
2021-10-05 18:27:52 +0200

    removed webserver + added temp sensor for 3v

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <ESPAsyncWebServer.h>
 #include "env.h"
 #include "flowerData.h"
 #include "tempData.h"
@@ -17,7 +16,7 @@ using namespace ENV;
 #define TEMP_SENSOR 12
 
 
-#define READ_INTERVAL 10000
+#define READ_INTERVAL 60000
 #define SENSOR_01_NAME "bedroom-ficus"
 #define SENSOR_02_NAME "bedroom-palm"
 #define TEMP_SENSOR_NAME "bedroom-temp"
@@ -27,25 +26,9 @@ int moisture_01 = 0;
 int moisture_02 = 0;
 float temperature = 0.0f;
 
-AsyncWebServer server(80);
+
 DHT_Unified dht(TEMP_SENSOR, DHT11);
 
-
-// REST endpoint for requesting current Sensor data
-void OnDataRequested(AsyncWebServerRequest *request) {
-
-    FLC::FlowerData data_01(SENSOR_01_NAME, moisture_01);
-    FLC::FlowerData data_02(SENSOR_02_NAME, moisture_02);
-    FLC::TempData temp_data(TEMP_SENSOR_NAME, temperature);
-
-    String response = "[" + data_01.ToJson() + "," + data_02.ToJson() + "," + temp_data.ToJson() + "]";
-
-    request->send(200, "application/json", response);
-}
-
-void OnNotFound(AsyncWebServerRequest *request) {
-    request-> send(404, "appliction/json", "Not found");
-}
 
 
 void setup() {
@@ -72,17 +55,11 @@ void setup() {
 
     Serial.println("Connected to network. IPv4:");
     Serial.println(WiFi.localIP());
-
-    // Basic json web api
-    server.on("/", HTTP_GET, OnDataRequested);
-    server.onNotFound(OnNotFound);
-    server.begin();
 }
 
 
 void loop() {   
-    delay(READ_INTERVAL);
-
+    
     sensors_event_t event;
     dht.temperature().getEvent(&event);
     
@@ -95,13 +72,15 @@ void loop() {
         FLC::FlowerData data_01(SENSOR_01_NAME, moisture_01);
         FLC::FlowerData data_02(SENSOR_02_NAME, moisture_02);
 
-        data_01.Post();
-        data_02.Post();
+        //data_01.Post();
+        //data_02.Post();
 
         if (isnan(event.temperature)) Serial.println(F("Error reading temperature!"));
         else {
             FLC::TempData temp_data(TEMP_SENSOR_NAME, temperature);
-            temp_data.Post();
+            //temp_data.Post();
         }
     }   
+
+    delay(READ_INTERVAL);
 }
